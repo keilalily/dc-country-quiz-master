0b65a53ff34c7ab11295cce94fd374fcf15867ac
Kyla Marie Angeles <kylamarieangeles@gmail.com>
2025-06-12 19:16:23 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <>
       <main
-        className='relative background w-full min-h-screen flex flex-col items-center justify-center p-8 overflow-y-auto'
+        className='background w-full min-h-screen flex flex-col items-center justify-center p-8 overflow-y-auto'
       >
         <Quiz />
       </main>
-      <footer className='absolute bottom-4 flex flex-col items-center justify-center text-center text-primary/50 text-xs'>
+      <footer className='flex flex-col items-center justify-center text-center text-primary/50 text-xs'>
         <p>Coded by <a href="https://kylamarieangeles.vercel.app/" target="_blank" className='hover:text-primary'>Kyla Marie Angeles</a></p>
         <p>Challenge by <a href="https://www.devchallenges.io?ref=challenge" target="_blank" className='hover:text-primary'>devChallenges.io</a>.</p>
       </footer>
